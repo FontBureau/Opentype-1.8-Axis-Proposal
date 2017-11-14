43a34570ffdc8bcd0e30ea469e89eb1bedd3e840
Chris Lewis <chrissam42@gmail.com>
2017-11-14 14:32:21 -0500

    Convert to PHP and separate header and footer

---
+++ foot.php
@@ -0,0 +1,10 @@
+		
+		<footer class="footer-global">
+			<p>&copy; Font Bureau <?= date("Y") ?>. All Rights Reserved.</p>
+		</footer>
+		
+	</div>
+</div>
+
+</body>
+</html>
---
+++ head.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title><?php if (!empty($title)) echo "$title — "; ?>Font Bureau OpenType 1.8 Variations Axes Proposals</title>
+
+	<meta name="viewport" content="initial-scale=1">
+	
+	<link rel="stylesheet" href="https://www.typenetwork.com/assets_content/css/adobe-blank.css">
+	<link rel="stylesheet" href="https://www.typenetwork.com/assets_content/css/fonts-momentum-sans.css">
+	<link rel="stylesheet" href="https://www.typenetwork.com/assets_content/css/icon_font.css">
+	<link rel="stylesheet" href="https://www.typenetwork.com/assets_content/css/reset.css">
+	<link rel="stylesheet" href="https://www.typenetwork.com/assets_content/css/style.css">
+	<link rel="stylesheet" href="https://www.typenetwork.com/assets_content/css/lightbox.css">
+	<link rel="stylesheet" href="https://www.typenetwork.com/assets_content/css/awesomplete.css">	
+
+	<style>
+		body, h1, h2, h3, h4, h5, h6, p {
+			font-family: Verdana, sans-serif !important;
+		}
+	</style>
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-78231280-2"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	
+	  gtag('config', 'UA-78231280-2');
+	</script>
+
+	<script src='https://www.typenetwork.com/assets_content/js/libs/jquery-1.11.0.min.js'></script>
+	<script src="https://www.typenetwork.com/assets_content/js/functions.js"></script>
+</head>
+<body>
+	
+<div class="wrapper">
+	
+	<header class="header-global">
+	</header>
+	
+	<aside class="content-filters">
+		<a class="content-filters-close" href="#">Close</a>
+		
+		<h2>Axes Proposal</h2>
+		
+		<h3 class="content-filters-group-collapsed"><a href="#xtra">A. Primary Type Axes</a></h3>
+		<ul class="content-filters-collapsed">
+			<li><a href="#xtra">1. xtra</a></li>
+			<li><a href="#xopq">2. xopq</a></li>
+			<li><a href="#ytra">3. ytra</a></li>
+			<li><a href="#yopq">4. yopq</a></li>
+			<li><a href="#ytlc">5. ytlc</a></li>
+			<li><a href="#ytuc">6. ytuc</a></li>
+			<li><a href="#ytde">7. ytde</a></li>
+			<li><a href="#ytas">8. ytas</a></li>
+			<li><a href="#xtab">9. xtab</a></li>
+			<li><a href="#vrot">10. vrot</a></li>
+		</ul>
+		<h3 class="content-filters-group-collapsed"><a href="#udln">B. Treatment Axes</a></h3>
+		<ul class="content-filters-collapsed">
+			<li><a href="#udln">11. udln</a></li>
+			<li><a href="#shdw">12. shdw</a></li>
+			<li><a href="#refl">13. refl</a></li>
+			<li><a href="#otln">14. otln</a></li>
+			<li><a href="#engr">15. engr</a></li>
+			<li><a href="#embo">16. embo</a></li>
+		</ul>
+		<h3 class="content-filters-group-collapsed"><a href="#ytch">C. Non-Latin Axes</a></h3>
+		<ul class="content-filters-collapsed">
+			<li><a href="#ytch">17. ytch</a></li>
+			<li><a href="#xtch">18. xtch</a></li>
+		</ul>
+		<h3 class="content-filters-group-collapsed"><a href="#rxad">D. Motion Axes</a></h3>
+		<ul class="content-filters-collapsed">
+			<li><a href="#rxad">19. rxad</a></li>
+			<li><a href="#ryad">20. ryad</a></li>
+			<li><a href="#rsec">21. rsec</a></li>
+		</ul>
+		<h3 class="content-filters-group-collapsed"><a href="#vuid">E. Glyph Axes</a></h3>
+		<ul class="content-filters-collapsed">
+			<li><a href="#vuid">22. vuid</a></li>
+			<li><a href="#votf">23. votf</a></li>
+		</ul>
+		
+	</aside>
+
+	<div class="content-main">
+		
+		<a class="content-options-show-filters" href="#">Filters</a>
+		
